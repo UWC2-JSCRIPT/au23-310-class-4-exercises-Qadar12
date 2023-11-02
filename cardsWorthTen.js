1fc3185615351f2f8c6bf01b0fd715f8fddeafa7
qadar omar <omarqadar3@gmail.com>
2023-11-01 21:17:22 -0700

    done with foodIsCooked and working on cardsWorthTen

--- cardsWorthTen.js
+++ cardsWorthTen.js
@@ -11,7 +11,7 @@ const cards = [
   { val: 10, displayVal: "Jack", suit: "hearts" },
   { val: 10, displayVal: "Queen", suit: "hearts" },
   { val: 10, displayVal: "King", suit: "hearts" },
-  { val: 11, displayVal: "Ace", suit: "hearts" }
+  { val: 11, displayVal: "Ace", suit: "hearts" },
 ];
 
 /**
@@ -21,7 +21,6 @@ const cards = [
  * @param {array} cards
  * @return {string} displayVal
  */
-const cardsWorthTen = cards => {};
 
 console.log(cardsWorthTen(cards));
 // should return/log "10, Jack, Queen, King"
